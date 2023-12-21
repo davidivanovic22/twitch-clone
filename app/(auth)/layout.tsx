1eb1ccb4d335c458c54f2ee6dcc10a73eb20bbd7
David Ivanovic <david.ivanovic@serengetitech.com>
2023-12-21 21:50:46 +0100

    init commit

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,17 @@
+import { ReactNode } from "react";
+import { Logo } from "./_components/logo";
+
+const AuthLayout = ({
+  children
+}: {
+  children: ReactNode;
+}) => {
+  return (
+    <div className="h-full flex flex-col items-center justify-center space-y-6">
+      <Logo />
+      {children}
+    </div>
+  );
+};
+
+export default AuthLayout;
